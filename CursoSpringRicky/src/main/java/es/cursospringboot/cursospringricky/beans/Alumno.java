5d1ff9452b445ef2099470dc0acce738a3c48e6a
rmartin04 <ricarditomartin1979+1@gmail.com>
2025-06-03 17:03:47 +0200

    primeros ejemplos con api rest

---
+++ CursoSpringRicky/src/main/java/es/cursospringboot/cursospringricky/beans/Alumno.java
@@ -0,0 +1,55 @@
+package es.cursospringboot.cursospringricky.beans;
+
+import java.util.List;
+
+public class Alumno {
+	private int id;
+    private String nombre;
+    private String email;
+    private List<String> asignaturas;
+    private double notaMedia;
+	public Alumno() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Alumno(int id, String nombre, String email, List<String> asignaturas, double notaMedia) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.email = email;
+		this.asignaturas = asignaturas;
+		this.notaMedia = notaMedia;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public List<String> getAsignaturas() {
+		return asignaturas;
+	}
+	public void setAsignaturas(List<String> asignaturas) {
+		this.asignaturas = asignaturas;
+	}
+	public double getNotaMedia() {
+		return notaMedia;
+	}
+	public void setNotaMedia(double notaMedia) {
+		this.notaMedia = notaMedia;
+	}
+    
+	
+}
---
+++ CursoSpringRicky/src/main/java/es/cursospringboot/cursospringricky/rest/AlumnoRestController.java
@@ -0,0 +1,68 @@
+package es.cursospringboot.cursospringricky.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import es.cursospringboot.cursospringricky.beans.Alumno;
+
+
+
+@RestController
+@RequestMapping("/alumnos")
+public class AlumnoRestController {
+	
+	private List<Alumno> lista = new ArrayList<>();
+	
+	@GetMapping
+    public List<Alumno> obtenerAlumnos() {
+		System.out.println("Devolviendo lista alumnos");
+        return lista;
+    }
+
+    @PostMapping
+    public Alumno crearAlumno(@RequestBody Alumno Alumno) {
+        lista.add(Alumno);
+        return Alumno;
+    }
+
+    @GetMapping("/{id}")
+    public Alumno obtenerPorId(@PathVariable int id) {
+    	Alumno alumno=null;
+        for (Alumno a : lista) {
+			if (a.getId()==id) {
+				alumno=a;
+			}
+		}
+        //return lista.stream().filter(u -> u.getId() == id).findFirst().orElse(null);
+        return alumno;
+    }
+
+    @PutMapping("/{id}")
+    public Alumno actualizarAlumno(@PathVariable int id, @RequestBody Alumno AlumnoActualizado) {
+        Alumno u = obtenerPorId(id);
+        if (u != null) {
+            u.setNombre(AlumnoActualizado.getNombre());
+            u.setEmail(AlumnoActualizado.getEmail());
+        }
+        return u;
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminarAlumno(@PathVariable int id) {
+    	for (Alumno a : lista) {
+			if (a.getId()==id) {
+				lista.remove(a);
+			}
+		}
+        //lista.removeIf(u -> u.getId() == id);
+    }
+}
